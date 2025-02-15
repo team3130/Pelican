b5696489bf02291a444f32085a8dfa8d5a136748
Umar K <umarkhanayatampa@gmail.com>
2025-02-15 11:13:44 -0600

    lowkey working code for elevator, intake, manip
    blue twilight comp

--- src/main/java/frc/robot/commands/Elevator/GoToL4Basic.java
+++ src/main/java/frc/robot/commands/Elevator/GoToL4Basic.java
@@ -2,44 +2,52 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.Chassis;
+package frc.robot.commands.Elevator;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.Chassis;
+import frc.robot.subsystems.Elevator;
 
 /** An example command that uses an example subsystem. */
-public class RotateToSetpoint extends Command {
+public class GoToL3Basic extends Command {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final Chassis chassis;
+  private final Elevator elevator;
 
   /**
    * Creates a new ExampleCommand.
    *
-   * @param chassis The subsystem used by this command.
+   * @param elevator The subsystem used by this command.
    */
-  public RotateToSetpoint(Chassis chassis) {
-    this.chassis = chassis;
+  public GoToL3Basic(Elevator elevator) {
+    this.elevator = elevator;
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(chassis);
+    addRequirements(elevator);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    chassis.turnToAngle(0);
+
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    if(elevator.getPosition() > elevator.getL3()) {
+      elevator.goDown();
+    }else if(elevator.getPosition() < elevator.getL3()) {
+      elevator.goUp();
+    }
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    elevator.stop();
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+      return elevator.getPosition() < elevator.getL3() + 5 && elevator.getPosition() > elevator.getL3() - 5;
   }
 }
