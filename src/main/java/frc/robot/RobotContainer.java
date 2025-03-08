f73cfb606583192f06f9dec5064ec42b7221400a
Rohan-Kuruvila <151589950+Rohan-Kuruvila@users.noreply.github.com>
2025-03-07 20:17:46 -0600

    Used Method TeleopDrive in RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -131,10 +131,7 @@ public class RobotContainer {
 
     // Note that X is defined as forward according to WPILib convention,
     // and Y is defined as to the left according to WPILib convention.
-    driveTrain.setDefaultCommand(
-            // Drivetrain will execute this command periodically
-            driveTrain.applyRequest(this::accelLimitVectorDrive)
-    );
+    driveTrain.setDefaultCommand(teleopDrive);
 
     // Run SysId routines when holding back/start and X/Y.
     // Note that each routine should be run exactly once in a single log.
