2ba8c5322c5ad66f3784422ce9c7f76aecabb966
Albert M Wright <albertmwright@gmail.com>
2025-03-18 14:54:32 -0500

    fixed LED stuff, Umar's stuff is still broken

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -29,6 +29,7 @@ import frc.robot.commands.Elevator.*;
 import frc.robot.commands.Manipulator.*;
 import frc.robot.sensors.Camera;
 import frc.robot.subsystems.*;
+import frc.robot.subsystems.LEDs;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
